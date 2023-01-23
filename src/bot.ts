08b3a31a5ba72a1bb2e837d090552d53cb914a6f
Agung Wicaksono <gungcaksono.id@gmail.com>
2023-01-23 23:41:08 +0800

    WIP: init start bot

--- src/bot.ts
+++ src/bot.ts
@@ -1,1 +1,13 @@
-console.log('hello world!');
+import { Client, Events, GatewayIntentBits } from 'discord.js';
+import * as dotenv from 'dotenv';
+
+dotenv.config({ path: require('find-config')('.env') });
+console.log('TOKEN: ', process.env.BOT_TOKEN);
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.once(Events.ClientReady, (x) => {
+  console.log(`Ready! Logged in as ${x.user.tag}`);
+});
+
+client.login(process.env.BOT_TOKEN);
