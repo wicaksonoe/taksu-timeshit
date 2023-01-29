8451c4933a7254819697fe119f9f2d06a194aa25
Agung Wicaksono <gungcaksono.id@gmail.com>
2023-01-29 17:51:28 +0800

    feat: create query param generator

---
+++ src/utils/generator.ts
@@ -0,0 +1,38 @@
+import moment from 'moment';
+import { UserSchema } from '../models/users';
+
+export interface GeneratorBaseResponse {
+  is_success: boolean;
+  message: string;
+}
+
+export async function GenerateTimeSheetUrl(
+  user: UserSchema,
+  date?: string
+): Promise<GeneratorBaseResponse> {
+  const fields = [
+    {
+      key: 'entry.2116052852',
+      value: user.name.replace(/\s/g, '+')
+    },
+    {
+      key: 'entry.532096719',
+      value: user.position.replace(/\s/g, '+')
+    },
+    {
+      key: 'entry.1369552271',
+      value: user.project.replace(/\s/g, '+')
+    },
+    {
+      key: 'entry.1060472253',
+      value: date ?? moment().format('YYYY-MM-DD')
+    }
+  ];
+
+  const parsedQuery = fields.map((x) => `${x.key}=${x.value}`).join('&');
+
+  return {
+    is_success: true,
+    message: parsedQuery
+  };
+}
